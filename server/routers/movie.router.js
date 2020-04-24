382ae5c3c206c8c03af901a0e16823e39af02a10
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-24 18:52:30 -0500

    adds query to put route

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -37,12 +37,27 @@ router.get("/", (req, res) => {
 router.put("/edit", (req, res) => {
   //  id is a required query param
   if (req.query.id) {
+    // expects body object as { title: "Scott Pilgrim", description: "lorem ipsum" }
     const title = req.body.title;
     const description = req.body.description;
-    console.log(title, description);
-    res.sendStatus(200);
+    const queryText = `UPDATE movies
+    SET "title" = '$1',
+        "description" = '$2'
+    WHERE "id" = $3;
+    `;
+    const queryParams = [title, description, req.query.id];
+
+    pool
+      .query(queryText, queryParams)
+      .then((response) => {
+        res.sendStatus(200); //  send 200 if updated
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500); //  send 500 if server error
+      });
   } else {
-    res.sendStatus(400);
+    res.sendStatus(400); //  send 400 if missing params
   }
 });
 module.exports = router;
