41e11821c3811aec6138a03f9c0504557e7d1cdf
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-24 18:29:18 -0500

    adds filtered default get route
    get route has queryparam id to select a movie by its movie_id

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -4,16 +4,34 @@ const pool = require("../modules/pool");
 
 module.exports = router;
 
-router.get("/all", (req, res) => {
-  const queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id ORDER BY movie_genres.id ASC;`;
+router.get("/", (req, res) => {
+  let queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id ORDER BY movie_genres.movie_id ASC;`;
+  //  default case for query, no params
 
-  pool
-    .query(queryText)
-    .then((response) => {
-      res.send(response.rows);
-    })
-    .catch((err) => {
-      console.warn("@movies/all", err);
-      res.sendStatus(500);
-    });
+  // one query param -> id  = table movies_genres movie_id value.
+  if (req.query.id) {
+    // if there is a query param for the movie id, find it
+    queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id WHERE movies.id = $1;`;
+
+    pool
+      .query(queryText, [req.query.id])
+      .then((response) => {
+        res.send(response.rows);
+      })
+      .catch((err) => {
+        console.warn("@movies/all", err);
+        res.sendStatus(500);
+      });
+  } else {
+    // if not get everything
+    pool
+      .query(queryText, [])
+      .then((response) => {
+        res.send(response.rows);
+      })
+      .catch((err) => {
+        console.warn("@movies/all", err);
+        res.sendStatus(500);
+      });
+  }
 });
