25b9e699956099b31b4d425af8b66e3e6f6eae97
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-24 19:29:38 -0500

    made specific routers

--- server/routers/movie.router.js
+++ server/routers/movie.router.js
@@ -2,39 +2,23 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
-//  get all movies and their genres
-router.get("/all", (req, res) => {
-  let queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id ORDER BY movie_genres.movie_id ASC;`;
-  //  default case for query, no params
+//  get all the movies
+router.get("/", (req, res) => {
+  const queryParams = `SELECT * FROM MOVIES ORDER BY "id" ASC;`;
 
-  // one query param -> id  = table movies_genres movie_id value.
-  if (req.query.id) {
-    // if there is a query param for the movie id, find it
-    queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id WHERE movies.id = $1;`;
+  pool
+    .query(queryParams)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log("@/movies", err);
 
-    pool
-      .query(queryText, [req.query.id])
-      .then((response) => {
-        res.send(response.rows);
-      })
-      .catch((err) => {
-        console.warn("@movies/all", err);
-        res.sendStatus(500);
-      });
-  } else {
-    // if not get everything
-    pool
-      .query(queryText, [])
-      .then((response) => {
-        res.send(response.rows);
-      })
-      .catch((err) => {
-        console.warn("@movies/all", err);
-        res.sendStatus(500);
-      });
-  }
+      res.sendStatus(500);
+    });
 });
 
+//  edit a movie's title/description
 router.put("/edit", (req, res) => {
   //  id is a required query param
   if (req.query.id) {
---
+++ server/routers/movieGenres.router.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../modules/pool");
+
+//  get all the movies and their genres
+router.get("/", (req, res) => {
+  let queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id ORDER BY movie_genres.movie_id ASC;`;
+  //  default case for query, no params
+
+  // one query param -> id  = table movies_genres movie_id value.
+  if (req.query.id) {
+    // if there is a query param for the movie id, find it
+    queryText = `SELECT * FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id JOIN genres ON genres.id = movie_genres.genre_id WHERE movies.id = $1;`;
+
+    pool
+      .query(queryText, [req.query.id])
+      .then((response) => {
+        res.send(response.rows);
+      })
+      .catch((err) => {
+        console.warn("@movieGenres/", err);
+        res.sendStatus(500);
+      });
+  } else {
+    // if not get everything
+    pool
+      .query(queryText, [])
+      .then((response) => {
+        res.send(response.rows);
+      })
+      .catch((err) => {
+        console.warn("@movieGenres/", err);
+        res.sendStatus(500);
+      });
+  }
+});
+
+module.exports = router;
