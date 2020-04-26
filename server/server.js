9197a9fb84de1233b761c1f123b878a2b5644da6
Cole Chelton <59346941+cchelton@users.noreply.github.com>
2020-04-26 15:12:11 -0500

    Revert "Feature editing"

--- server/server.js
+++ server/server.js
@@ -1,25 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("build"));
+app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 
-const movieRouter = require("./routers/movie.router");
-app.use("/api/movies", movieRouter);
-
-const genreRouter = require("./routers/genre.router");
-app.use("/api/genres", genreRouter);
-
-const movieGenresRouter = require("./routers/movieGenres.router");
-app.use("/api/movieGenres", movieGenresRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-  console.log("Listening on port: ", port);
+    console.log('Listening on port: ', port);
 });
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,12 @@
-import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import ListPage from "../../views/ListPage";
-import DetailsPage from "../../views/DetailsPage";
-import EditPage from "../../views/EditPage";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>Movie Sagas</h1>
-        </header>
-        <Router>
-          <Route exact path="/" component={ListPage} />
-          <Route path="/details/:id" component={DetailsPage} />
-          <Route path="/edit/:id" component={EditPage} />
-        </Router>
+        <p>Empty Page</p>
       </div>
     );
   }
